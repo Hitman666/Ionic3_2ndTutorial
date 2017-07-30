5267715c4f02000610c8ad6c898b22e9f6170e5e
Nikola Brežnjak <nikola.breznjak@gmail.com>
2017-07-30 16:39:56 +0200

    Copied src folder content from Ionic Creator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { CalculatorPage } from '../pages/calculator/calculator';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    CalculatorPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +20,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    CalculatorPage
   ],
   providers: [
     StatusBar,
