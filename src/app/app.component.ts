0f4df24ec5cf806b6b33c6e426b487818f22acb4
Nikola Brežnjak <nikola.breznjak@gmail.com>
2017-07-31 09:13:37 +0200

    Finished project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,22 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-
-
 import { CalculatorPage } from '../pages/calculator/calculator';
 
-
-
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
-export class MyApp {
-  
-    rootPage:any = CalculatorPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
-  
+export class MyApp {
+    rootPage: any = CalculatorPage;
+
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+        });
+    }
+
 }
--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -2,12 +2,26 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-calculator',
-  templateUrl: 'calculator.html'
+    selector: 'page-calculator',
+    templateUrl: 'calculator.html'
 })
 export class CalculatorPage {
+    result = '';
 
-  constructor(public navCtrl: NavController) {
-  }
-  
+    btnClicked(btn) {
+        if (btn == 'C') {
+            this.result = '';
+        }
+        else if (btn == '=') {
+            this.result = eval(this.result);
+        }
+        else {
+            this.result += btn;
+        }
+    }
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
 }
